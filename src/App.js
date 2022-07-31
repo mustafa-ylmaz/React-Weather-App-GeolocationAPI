87dcb13361edafc45510d5406a6f56b96acbe947
Mustafaylmaz <mustafaa.ylmaz@gmail.com>
2022-07-31 15:26:02 +0300

    .

--- src/App.js
+++ src/App.js
@@ -4,49 +4,22 @@ import DropdownButton from 'react-bootstrap/DropdownButton';
 import Card from './components/Card';
 import axios from 'axios';
 
-const geolocationAPI = navigator.geolocation;
+
 const  cities  = require('./iller.js');
-
-
-
 const APIKEY = '9650d51c3088b5c10d7eea04f6f313a7';
-//
+
 
 function App() {
-  
   const [data, setData] = useState(null);
   const [city, setCity] = useState('Istanbul');
 
   const [lat, setLat] = useState(null);
   const [long, setLong] = useState(null);
-  
-  const [error, setError] = useState(null);
-  
-  const getUserCoordinates = () => {
-    if (!geolocationAPI) {
-      setError('Geolocation API is not available in your browser!')
-    } else {
-      geolocationAPI.getCurrentPosition((position) => {
-        const { coords } = position;
-        setLat(coords.latitude);
-        setLong(coords.longitude);
-      }, (error) => {
-        setError('Something went wrong getting your position!')
-      })
-    }
-    if (!geolocationAPI) {
-      setError('Geolocation API is not available in your browser!')
-    }
-    else {
-      geolocationAPI.getCurrentPosition((position) => {
-        const { coords } = position;
-        setLat(coords.latitude);
-        setLong(coords.longitude);
-      },
-      (error) => {
-        setError('Something went wrong getting your position!')
-      })} }
-      
+
+
+  navigator.geolocation.getCurrentPosition(function(position) {
+    setLat(position.coords.latitude)
+    setLong(position.coords.longitude) })
 
 
   const listCity = cities.sort((a,b) => a.id-b.id);
@@ -56,7 +29,7 @@ function App() {
   
   const fetchdata = async() => { 
     if(lat) { 
-       await axios(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${APIKEY}`)
+       await axios(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&cnt=30&appid=${APIKEY}`)
       .then((res) => setData(res.data));
       console.log(data);
 
@@ -70,11 +43,25 @@ function App() {
 
 
     useEffect(() => {
-      
+      let unix_timestamp = 1659290400
 
-      fetchdata();
-      getUserCoordinates();
+      var date = new Date(unix_timestamp * 1000);
+
+      var hours = date.getHours();
+      // Minutes part from the timestamp
+      var minutes = "0" + date.getMinutes();
+      // Seconds part from the timestamp
+      var seconds = "0" + date.getSeconds();
       
+      // Will display time in 10:30:23 format
+      var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+      
+      console.log(date);
+      fetchdata();
+      
+  
+
+
       
       
     }, [city,lat]) 
